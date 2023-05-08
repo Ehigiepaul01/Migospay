1932875e8d11a064d961996dcc478ac8676e0efe
Ehigiepaul01 <paulehigie5@gmail.com>
2023-05-08 13:35:21 +0100

    finished working on the FAQ page

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('active');
   navList.classList.toggle('show');
 });
+
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', event => {
+    const parent = event.target.closest('.accordion-item');
+    parent.classList.toggle('active');
+  });
+});

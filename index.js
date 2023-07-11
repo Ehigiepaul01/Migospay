737afd9776fcad30fe7cc78a3b8b565b87055604
Ehigiepaul01 <paulehigie5@gmail.com>
2023-07-11 21:44:37 +0100

    made some little update

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ hamburgerMenu.addEventListener('click', () => {
   navList.classList.toggle('show');
 });
 
+
+
 const accordionHeaders = document.querySelectorAll('.accordion-header');
 
 accordionHeaders.forEach(header => {
@@ -14,3 +16,37 @@ accordionHeaders.forEach(header => {
     parent.classList.toggle('active');
   });
 });
+
+const carousel = document.querySelector(".carousel");
+const slides = Array.from(carousel.getElementsByClassName("slide"));
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+let slideWidth = slides[0].offsetWidth;
+let currentIndex = 0;
+
+// Update slide width on window resize
+window.addEventListener("resize", () => {
+  slideWidth = slides[0].offsetWidth;
+  carousel.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+});
+
+// Handle next button click
+nextBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  carousel.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+});
+
+// Handle previous button click
+prevBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  carousel.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+});
+
+
+
+
+
+
+
+
